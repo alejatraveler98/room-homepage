26833e74c93c2e27b1bd35da1b55a7f01f5d6d07
alejatraveler98 <alrg1720@gmail.com>
2020-10-24 11:58:10 -0500

    Fix en imagenes y js
    Se agrego atributos pictures y sources para que de esta manera podamos crear un responsive cambiando la imagen
    segun el formato si es para móvil o para desktop
    
    Si tenemos que aclarar que el cambio de estilos hace falta pasarlos a sass y compilar en la sección de about-furniture
    colocar el padding diferente para que se ajuste según el tamaño de la pantalla

--- assets/js/actions.js
+++ assets/js/actions.js
@@ -1,7 +1,8 @@
 const left= document.getElementById('left')
 const right= document.getElementById('right')
 const title= document.getElementById('title')
-const img= document.getElementById('img-hero')
+const imgDesktop= document.getElementById('img-hero-desktop')
+const imgMobile = document.getElementById('img-hero-mobile')
 const p= document.getElementById('paragraph')
 let slide_index = 0
 
@@ -9,18 +10,22 @@ let elements = [
         {
             title:"Discover innovative ways to decorate",
             src:"/assets/img/desktop-image-hero-1.jpg",
-            p: "We provide unmatched quality, comfort, and style for propertyowners acrossthe country.Our experts combine form andnfunction in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love."
-            
+            p: "We provide unmatched quality, comfort, and style for propertyowners acrossthe country.Our experts combine form andnfunction in bringing your vision to life. Create a room in your own style with our collection and make your property a reflection of you and what you love.",
+            srcmobile:"/assets/img/mobile-image-hero-1.jpg"
         },
         {
             title:"We are available all across the globe",
             src:"/assets/img/desktop-image-hero-2.jpg",
-            p:"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today."
+            p:"With stores all over the world, it's easy for you to find furniture for your home or place of business. Locally, we’re in most major cities throughout the country. Find the branch nearest you using our store locator. Any questions? Don't hesitate to contact us today.",
+            srcmobile:"/assets/img/mobile-image-hero-2.jpg"
+
         },
         {
             title:"Manufactured with the best materials",
             src:"/assets/img/desktop-image-hero-3.jpg",
-            p:"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office."
+            p:"Our modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.",
+            srcmobile:"/assets/img/mobile-image-hero-3.jpg"
+
         }
 ]
 const Action = (i) => {
@@ -28,9 +33,10 @@ const Action = (i) => {
     if(i >= elements.length) slide_index=0
     if(i < 0 ) {slide_index= elements.length - 1 }
     title.textContent=`${elements[slide_index].title}`
-    img.src=`${elements[slide_index].src}`
+    imgDesktop.src=`${elements[slide_index].src}`
+    imgMobile.srcset= `${elements[slide_index].srcmobile}`
     p.textContent=`${elements[slide_index].p}`
-    img.classList.toggle('animation')
+    imgDesktop.classList.toggle('animation')
 }
 
 
